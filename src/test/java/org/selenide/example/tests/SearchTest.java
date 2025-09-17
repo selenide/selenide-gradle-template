deb16bf6d49f9bb4a659f76795e3ec50b1f1b568
Andrei Solntsev <andrei.solntsev@gmail.com>
2025-09-17 11:30:47 +0300

    update test after a minor site redesign

--- src/test/java/org/selenide/example/tests/SearchTest.java
+++ src/test/java/org/selenide/example/tests/SearchTest.java
@@ -7,7 +7,7 @@ import org.selenide.example.tests.pagewidgets.UserTagsWidget;
 import org.selenide.example.tests.pagewidgets.Users;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.partialText;
+import static com.codeborne.selenide.Condition.partialTextCaseSensitive;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.TextCheck.FULL_TEXT;
 
@@ -33,6 +33,6 @@ public class SearchTest {
         // Assert
         Users results = new Users();
         results.found.shouldHave(sizeGreaterThan(10));
-        results.getResult(0).shouldHave(partialText("/ estonia"));
+        results.getResult(0).shouldHave(partialTextCaseSensitive("estonia"));
     }
 }
